3041ce86e802b37f67fc585255bdac5b0335efde
AlvaroOrquera <alvarosop23@gmail.com>
2024-01-23 11:01:08 -0300

    arregle las cagadas del fede....

--- src/main/java/com/mindhub/wireit/configurations/EnviromentConfig.java
+++ src/main/java/com/mindhub/wireit/configurations/EnviromentConfig.java
@@ -1,4 +1,4 @@
-package com.mindhub.wireit.Configurations;
+package com.mindhub.wireit.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
