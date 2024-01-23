f5a8c9daf28324b83c54b7cdab910429a1be9e6c
AlvaroOrquera <alvarosop23@gmail.com>
2024-01-23 11:03:26 -0300

    se arreglaron cositas

--- src/main/resources/configurations/EnviromentConfig.java
+++ src/main/resources/configurations/EnviromentConfig.java
@@ -1,4 +1,4 @@
-package com.mindhub.wireit.configurations;
+package configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
