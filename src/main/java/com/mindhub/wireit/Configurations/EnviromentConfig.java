c91d46f9223ae1049cbb800a404cf771bbc3ad4e
fedeval98 <fede.val9806@gmail.com>
2024-01-23 09:48:28 -0300

    corregido error en configurations

--- src/main/java/com/mindhub/wireit/Configurations/EnviromentConfig.java
+++ src/main/java/com/mindhub/wireit/Configurations/EnviromentConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @PropertySource("classpath:.env")
 public class EnviromentConfig {
 }
