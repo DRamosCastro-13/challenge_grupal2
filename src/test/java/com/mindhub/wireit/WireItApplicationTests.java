9a05e29667aabc7a3141cabf2899de6aa29fc20f
fedeval98 <fede.val9806@gmail.com>
2024-01-24 12:35:58 -0300

    Primer deploy de prueba

--- src/test/java/com/mindhub/wireit/WireItApplicationTests.java
+++ src/test/java/com/mindhub/wireit/WireItApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WireItApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
