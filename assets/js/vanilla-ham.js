1093a4da7ad6866f15bd00cbff6f303956a5ad85
danieloka <danirc01@gmail.com>
2024-01-25 12:24:17 -0500

    hamburguer menu

---
+++ assets/js/vanilla-ham.js
@@ -0,0 +1,13 @@
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
