1cf55f34488b33758c2ace345df3ad1c77592e4d
fedeval98 <fede.val9806@gmail.com>
2024-01-26 13:57:05 -0300

    modificado .env

--- src/main/java/com/mindhub/wireit/configurations/EnviromentConfig.java
+++ src/main/java/com/mindhub/wireit/configurations/EnviromentConfig.java
@@ -3,7 +3,7 @@ package com.mindhub.wireit.configurations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-//@Configuration
-//@PropertySource("classpath:.env")
+@Configuration
+@PropertySource("file:/etc/secrets/.env")
 public class EnviromentConfig {
 }
